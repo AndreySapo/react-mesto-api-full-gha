342f58eb70e61f4662b25651f392e1096a20d13b
AndreySapo <108719029+AndreySapo@users.noreply.github.com>
2023-03-22 21:11:16 +0800

    добавлен краш-тест

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,11 @@ mongoose.connect(SERVER, { useNewUrlParser: true });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post(
   '/signin',
   celebrate({
